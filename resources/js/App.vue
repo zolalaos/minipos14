71afbc9fff4f620c5ddc9285614b553c5939fbc3
zolalaos <anousak2011@gmil.com>
2024-01-16 22:17:53 +0700

    first commit"

---
+++ resources/js/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>Helo Vue App ok</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Minipos14App',
+
+    data() {
+        return {
+
+        };
+    },
+
+    mounted() {
+
+    },
+
+    methods: {
+
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>Home Vue</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Minipos14Home',
+
+    data() {
+        return {
+
+        };
+    },
+
+    mounted() {
+
+    },
+
+    methods: {
+
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
